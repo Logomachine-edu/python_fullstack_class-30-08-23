cfd0b1fa95fd233030c1cff7e174c056d50d6ecc
BSN-88 <nevigramon88@gmail.com>
2023-09-10 21:30:04 +0300

    с чистого листа

---
+++ first_file.py
@@ -0,0 +1,9 @@
+# Мой первый файл на пайтон
+<<<<<<< HEAD
+print("Hello, dog 1")
+print("Hello, dog 2")
+print("Hello, dog 3")
+# этот код работает исправно!!
+=======
+print("Hello, dog")
+>>>>>>> 5d41353 (Hello, dog)
